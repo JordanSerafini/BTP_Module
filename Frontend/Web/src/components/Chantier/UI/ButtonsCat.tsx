c0b83ccc87d81d88b9b2202d6ff6db147f69e611
JordanS <jordanserafini.74@gmail.com>
2024-12-30 17:10:24 +0100

    working

--- Frontend/Web/src/components/Chantier/UI/ButtonsCat.tsx
+++ Frontend/Web/src/components/Chantier/UI/ButtonsCat.tsx
@@ -9,7 +9,7 @@ function ButtonsCat({ title, content, setSelectedChantier, selectedChantier }: B
   return (
     <div 
     onClick={()=>setSelectedChantier(content)}
-    className={`w-fit px-4 py-2 h-10 flex items-center justify-center cursor-pointer tracking-widest rounded-full ${content === selectedChantier ? "bg-gray-800 text-white " : "bg-gray-300 text-white"}`}
+    className={`w-fit px-4 py-2 h-10 flex items-center justify-center cursor-pointer rounded-full ${content === selectedChantier ? "bg-gray-800 text-white " : "bg-gray-300 text-white italic"}`}
     >{title}</div>
   )
 }
